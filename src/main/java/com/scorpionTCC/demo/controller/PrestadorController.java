488448f254fba570fbc6d0ddb50175c3c04debdc
ThiagoCL16 <thiagocavalcantelopes16@gmail.com>
2026-09-25 10:58:04 -0300

    Métodos de salvar, buscar por id, atualizar e excluir prestador

--- src/main/java/com/scorpionTCC/demo/controller/PrestadorController.java
+++ src/main/java/com/scorpionTCC/demo/controller/PrestadorController.java
@@ -1,17 +1,18 @@
 package com.scorpionTCC.demo.controller;
 
 import com.scorpionTCC.demo.entity.Contratante;
+import com.scorpionTCC.demo.entity.PrestadorServico;
 import com.scorpionTCC.demo.entity.Vaga;
 import com.scorpionTCC.demo.repository.PrestadorRepository;
+import com.scorpionTCC.demo.service.ContratanteService;
 import com.scorpionTCC.demo.service.PrestadorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/prestadores")
@@ -19,4 +20,37 @@ public class PrestadorController {
     @Autowired
     private PrestadorService prestadorService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<PrestadorServico> buscarPrestadorPorId(@PathVariable Long id){
+        Optional<PrestadorServico> prestador = prestadorService.findById(id);
+        if(prestador.isPresent())
+            return ResponseEntity.ok(prestador.get());
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<PrestadorServico> cadastrar(@RequestBody PrestadorServico p){
+        PrestadorServico prestador = prestadorService.save(p);
+        return ResponseEntity.ok(prestador);
+    }
+
+    @PatchMapping("/atualizar/{id}")
+    public ResponseEntity<PrestadorServico> atualizarPerfil(@RequestBody Map<String, Object> campos, @PathVariable Long id) {
+        PrestadorServico prestador = prestadorService.update(id, campos);
+
+        if(prestador == null)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(prestador);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<PrestadorServico> deletarPerfil(@PathVariable Long id){
+        PrestadorServico prestador = prestadorService.deleteById(id);
+        if(prestador == null)
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(prestador);
+    }
 }
--- src/main/java/com/scorpionTCC/demo/service/PrestadorService.java
+++ src/main/java/com/scorpionTCC/demo/service/PrestadorService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -19,4 +20,58 @@ public class PrestadorService {
     @Autowired
     PrestadorRepository prestadorRepository;
 
+    public PrestadorServico save(PrestadorServico prestador){
+        PrestadorServico p = prestadorRepository.save(prestador);
+        return p;
+    }
+
+    public Optional<PrestadorServico> findById(Long id){
+        return prestadorRepository.findById(id);
+    }
+
+    public PrestadorServico update(Long id, Map<String, Object> campos){
+        Optional<PrestadorServico> optionalPrestador = prestadorRepository.findById(id);
+        PrestadorServico prestador;
+        if(optionalPrestador.isPresent())
+            prestador = optionalPrestador.get();
+        else
+            return null;
+
+        campos.forEach((nomeCampo, valorCampo) -> {
+            if (nomeCampo.equals("cidade"))
+                prestador.setCidade((String) valorCampo);
+            else if (nomeCampo.equals("avaliacoes")) {
+                // Se o valor for qualquer tipo de número, converte com segurança para Double
+                if (valorCampo instanceof Number) {
+                    prestador.setAvaliacoes(((Number) valorCampo).doubleValue());
+                }
+            } else if (nomeCampo.equals("nome"))
+                prestador.setNome((String) valorCampo);
+            else if (nomeCampo.equals("estado"))
+                prestador.setEstado((String) valorCampo);
+            else if (nomeCampo.equals("cep"))
+                prestador.setCep((String) valorCampo);
+            else if (nomeCampo.equals("anexos"))
+                prestador.setAnexos((String) valorCampo);
+            else if (nomeCampo.equals("descricao"))
+                prestador.setDescricao((String) valorCampo);
+            else if (nomeCampo.equals("tipoAssinatura"))
+                prestador.setTipoAssinatura((Boolean) valorCampo);
+
+        });
+
+        return prestadorRepository.save(prestador);
+    }
+
+    public PrestadorServico deleteById(Long id){
+        Optional<PrestadorServico> optionalPrestador = prestadorRepository.findById(id);
+        PrestadorServico prestador;
+
+        if(!optionalPrestador.isPresent())
+            return null;
+        else
+            prestador = optionalPrestador.get();
+        prestadorRepository.delete(prestador);
+        return prestador;
+    }
 }
